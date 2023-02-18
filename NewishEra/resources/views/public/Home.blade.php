cc368b40f8c3f7c38b26001df84f8d9499e2e288
fahimprito <ahmmedahmmed75@gmail.com>
2023-02-18 16:16:42 +0600

    home temp

---
+++ NewishEra/resources/views/public/Home.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Newish Era</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+
+</head>
+<body>
+	<header>
+		<div class="logo">
+			<img src="logo.png" alt="Newish Era Logo">
+		</div>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Products</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+		<div class="search-bar">
+			<input type="text" placeholder="Search">
+			<button>Search</button>
+		</div>
+		<div class="cart-icon">
+			<a href="#"><img src="cart.png" alt="Cart Icon"></a>
+		</div>
+	</header>
+	<main>
+		<section class="hero">
+			<h1>Shop Our Collection</h1>
+			<p>Discover the latest trends and styles</p>
+			<button>Shop Now</button>
+		</section>
+		<section class="categories">
+			<h2>Shop by Category</h2>
+			<ul>
+				<li><a href="#">Clothing</a></li>
+				<li><a href="#">Shoes</a></li>
+				<li><a href="#">Accessories</a></li>
+				<li><a href="#">Home Decor</a></li>
+			</ul>
+		</section>
+		<section class="featured-products">
+			<h2>Featured Products</h2>
+			<div class="product">
+				<img src="product1.png" alt="Product Image">
+				<h3>Product 1</h3>
+				<p>$19.99</p>
+				<button>Add to Cart</button>
+			</div>
+			<div class="product">
+				<img src="product2.png" alt="Product Image">
+				<h3>Product 2</h3>
+				<p>$29.99</p>
+				<button>Add to Cart</button>
+			</div>
+			<div class="product">
+				<img src="product3.png" alt="Product Image">
+				<h3>Product 3</h3>
+				<p>$39.99</p>
+				<button>Add to Cart</button>
+			</div>
+		</section>
+		<section class="testimonials">
+			<h2>What Our Customers Say</h2>
+			<div class="testimonial">
+				<p>"I love this store! The products are high quality and the customer service is excellent."</p>
+				<p>- Jane Doe</p>
+			</div>
+			<div class="testimonial">
+				<p>"I highly recommend this store. The selection is great and the prices are unbeatable."</p>
+				<p>- John Smith</p>
+			</div>
+		</section>
+	</main>
+	<footer>
+		<div class="links">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Products</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul
+            <p> &copy; 2023 Ecommerce Store. All rights reserved.</p>
+        </div>
+        <div class="social-media">
+            <a href="#"><img src="facebook.png" alt="Facebook Icon"></a>
+            <a href="#"><img src="twitter.png" alt="Twitter Icon"></a>
+            <a href="#"><img src="instagram.png" alt="Instagram Icon"></a>
+        </div>
+    </footer>
+
+</body>
+</html>
